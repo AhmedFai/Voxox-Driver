0491e87d706e0ff78a9034abc9fbbbf318f5f208
Mukul <mukulraw199517@gmail.com>
2018-02-07 19:08:02 +0530

    Voxox Driver

--- app/src/main/java/com/example/faizan/voxoxdriver/GoogleMapPOJO/Distance.java
+++ app/src/main/java/com/example/faizan/voxoxdriver/GoogleMapPOJO/Distance.java
@@ -1,38 +1,14 @@
 package com.example.faizan.voxoxdriver.GoogleMapPOJO;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 /**
- * Created by faizan on 2/5/2018.
+ * Created by Mai Thanh Hiep on 4/3/2016.
  */
-
 public class Distance {
-    @SerializedName("text")
-    @Expose
     public String text;
-    @SerializedName("value")
-    @Expose
-    private int value;
+    public int value;
 
     public Distance(String text, int value) {
         this.text = text;
-        this.value = value;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
         this.value = value;
     }
 }
